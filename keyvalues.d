c176af1205ee85716b42e47ec93a0a4e961c1f54
Steven Dwy <yoplitein+git@gmail.com>
2016-02-07 12:55:06 -0800

    Move Stack type into separate module

--- keyvalues.d
+++ keyvalues.d
@@ -6,6 +6,8 @@ import std.string;
 import std.traits;
 import std.uni;
 
+import stack;
+
 struct KeyValue
 {
     string key;
@@ -71,45 +73,6 @@ struct Token
     TokenType type;
     string value;
     Position position;
-}
-
-struct Stack(Type)
-{
-    Type[] data;
-    size_t index;
-    
-    @disable this();
-    
-    this(size_t initialSize = 1)
-    {
-        data.length = initialSize;
-    }
-    
-    void push(Type datum)
-    {
-        if(data.length <= index)
-            data.length *= 2;
-        
-        data[index++] = datum;
-    }
-    
-    Type pop()
-    {
-        if(empty)
-            throw new Exception("stack underflow");
-        
-        return data[--index];
-    }
-    
-    @property ref Type top()
-    {
-        return data[index - 1];
-    }
-    
-    @property bool empty()
-    {
-        return index == 0;
-    }
 }
 
 struct PositionTracker
